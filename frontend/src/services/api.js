a949f03c995db9735c376dc80e695058bd8fcc40
VANSHIKA SALEKAR <vanshikasalekar@VANSHIKAsMacBookAir.local>
2026-05-14 16:31:21 +0530

    Fixed API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://smartcomplaintsystem-h2u5.onrender.com",
+  baseURL: "https://smartcomplaintsystem-h2u5.onrender.com/api",
 });
 
 // Automatically attach token in every request
